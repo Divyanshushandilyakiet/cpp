a9eed62ea93c67bb601c6bcec98fc75b00535e88
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-04-18 17:06:20 +0530

    Add files via upload

---
+++ enter.cpp
@@ -0,0 +1,13 @@
+// Print entered number
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int number;
+    cout << "Enter the number:";
+    cin >> number;
+
+    cout << "You entered " << number;
+
+    return 0;
+}
