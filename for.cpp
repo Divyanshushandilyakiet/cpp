e380426f6ae99bb0c4dd7f3e0c16065de54fa1ab
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-04-22 10:33:22 +0530

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,11 @@
+// syntax of for loop
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
