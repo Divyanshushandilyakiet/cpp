459aba0626866a793767c1f259b4db5172fa439e
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-04-29 22:53:26 +0530

    Add files via upload

---
+++ pattern12.cpp
@@ -0,0 +1,24 @@
+// print number pattern
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n, i, j;
+    cout << "Enter a number:";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= i; j++)
+        {
+            cout << j
+                 << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
