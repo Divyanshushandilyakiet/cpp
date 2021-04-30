ebf829f6eb93586aba3dfbc0b597774193f789ef
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-04-30 19:17:24 +0530

    Add files via upload

---
+++ pattern14.cpp
@@ -0,0 +1,35 @@
+// print star pattern
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n, i, j;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (i = n; i >= 1; i--)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
