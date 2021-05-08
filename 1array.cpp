10a14ced804bbcb6521a4443e1f1a3bb1fe08d99
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-05-08 21:44:44 +0530

    Add files via upload

---
+++ 1array.cpp
@@ -0,0 +1,17 @@
+// Initilizing an array
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int arr[5];
+    arr[0]=12;
+    arr[1]=32;
+    arr[2]=42;
+    arr[3]=12;
+    arr[4]=435;
+    // int arr[5]={12,32,42,12,435};
+   cout<<arr[4]<<endl;
+    return 0;
+}
+
+
